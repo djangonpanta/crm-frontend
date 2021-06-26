0a2f1db1e7818ab6c1d217b3dca6bf3567d71167
Narayan Panta <djangonpanta@gmail.com>
2021-06-26 23:22:21 +1000

    breadcrumb added

---
+++ src/components/breadcrum/Breadcrumb.comp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Breadcrumb } from "react-bootstrap";
+
+export const PageBreadcrumb = ({ page }) => {
+  return (
+    <Breadcrumb className="test">
+      <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+
+      <Breadcrumb.Item active>{page}</Breadcrumb.Item>
+    </Breadcrumb>
+  );
+};
--- src/pages/dashboard/Dashboard.page.js
+++ src/pages/dashboard/Dashboard.page.js
@@ -2,9 +2,15 @@ import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { TicketTable } from "../../components/ticket-table/TicketTable.comp";
 import tickets from "../../assets/data/dummy-ticket.json";
+import { PageBreadcrumb } from "../../components/breadcrum/Breadcrumb.comp";
 export const Dashboard = () => {
   return (
     <Container>
+      <Row>
+        <Col>
+          <PageBreadcrumb page="Dashboard" />
+        </Col>
+      </Row>
       <Row>
         <Col className="text-center mt-5 mb-2">
           <Button
