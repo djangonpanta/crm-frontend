f718fa18fc1c1ca015505f4eb353766e4e76c61c
Narayan Panta <djangonpanta@gmail.com>
2021-06-26 23:00:39 +1000

    dashboard page added and fetch dummy data to create ticket table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { DefaultLayout } from "./components/layout/DefaultLayout";
+import { Dashboard } from "./pages/dashboard/Dashboard.page";
 import { Entry } from "./pages/entry/Entry.page";
-
 function App() {
   return (
     <div className="App">
       {/* <Entry /> */}
-      <DefaultLayout>send page component</DefaultLayout>
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
     </div>
   );
 }
